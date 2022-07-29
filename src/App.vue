7e4e0986f2a98e4a35231af7bbbf6a77dbbe1293
MAZEWEI <mazewei@kingsoft.com>
2022-07-29 18:31:34 +0800

    vue管理系统

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Left-nav></Left-nav>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+import LeftNav from './components/common/leftNav/index.vue'
+export default {
+    name: "App",
+    comments: {
+        leftNav
+    },
+    components: { LeftNav }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/common/leftNav/index.vue
@@ -0,0 +1,4 @@
+<template src="./_vue.pug" lang="pug">
+</template>
+<style src="./styles.css"></style>
+<script src="./_vue"></script>
